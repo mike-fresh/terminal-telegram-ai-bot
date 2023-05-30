b6712e49fcf1ca45a96a0147b8345e75e64ce3ee
Mike <mike-fresh.4444@vlx.me>
2023-05-30 23:41:44 +0200

    solve issue #1 with mutated vowels

--- modules/tools.py
+++ modules/tools.py
@@ -3,7 +3,15 @@ import re
 
 def clean_username(username: str) -> str:
     username = username.replace(" ", "_")
+    username = replace_umlauts(username)
     username = re.sub(r'[^a-zA-Z_-]', '', username)
     if len(username) > 64:
         username = username[:64]
     return username
+
+
+def replace_umlauts(text: str) -> str:
+    umlauts = {'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'Ä': 'Ae', 'Ö': 'Oe', 'Ü': 'Ue'}
+    for umlaut, replacement in umlauts.items():
+        text = text.replace(umlaut, replacement)
+    return text
